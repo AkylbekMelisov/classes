98231f06605a7bcef066d1b357132faf9f90b4d1
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-18 17:14:43 +0600

    first task1

--- week4/day1/classes.py
+++ week4/day1/classes.py
@@ -1,19 +1,20 @@
-class Student:
-    student_id = 0
+class Car:
 
-    def __init__(self,name,last_name,group):
-        self.name = name
-        self.last_name = last_name
-        self.group = group
-        Student.student_id +=1
+    def __init__(self,mark, color,model,year):
+        self.mark = mark
+        self.color = color
+        self.model = model
+        self.year = year
+        self.run = 0
 
-student1 = Student('akylbek','melisov','python')
-print(student1.student_id,student1.name)
-student2 = Student('jarkynai','satarova','python')
-print(student2.student_id,student2.name)
-student3 = Student('aigerim','kashkarbekova','python')
-print(student3.student_id,student3.name)
-student4 = Student('nurjanat','abaeva','python')
-print(student4.student_id,student4.name)
-student5 = Student('baiel','nurmatbek uulu','python')
-print(student5.student_id,student5.name)
+    def move(self,distance:int):
+        if distance > 0:
+            self.run += distance
+
+car1 = Car('bmv','back','x6','2020')
+car2 = Car('mercedes','pink','s 600','2021')
+car3 = Car('tesla','white','S 6','2021')
+car3.move(240)
+print(car3.mark,car3.model,car3.run)
+car3.move(-240)
+print(car3.mark,car3.model,car3.run)
