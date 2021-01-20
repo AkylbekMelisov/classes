886f4dd0a0d40f20c077b3d09fb10a3413dc1880
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-01-20 09:13:36 +0600

    game 3 (monster) version

--- week4/day2/classes_taask3.py
+++ week4/day2/classes_taask3.py
@@ -1,9 +1,9 @@
 class Person:
 
-    def __init__(self,name,health,mana,race):
+    def __init__(self,name,race):
         self.name = name
         self.race = race
-        if self.race == 'fary':
+        if self.race == 'fairy':
             self.health = 10
             self.mana = 2000
         elif self.race == 'goblin':
@@ -12,23 +12,23 @@ class Person:
         else:
             self.health = 150
             self.mana = 150
-        self.health = health
-        self.mana = mana
         self.power = 0
         self.damage = 0
         self.armor = 0
         self.level = 0
         self.exp = 0
+        self.position = 0
+        self.range = 1
         print('Персонаж успешно создан!')
 
-
     def description(self):
-        print(f"Имя: {self.name}, Здоровье: {self.health}, Мана: {self.mana},Раса: {self.race}, Сила: {self.power}, Урон: {self.damage}, Уровень: {self.level} Броня: {self.armor}")
+        print(f"Имя: {self.name}, Здоровье: {self.health}, Мана: {self.mana},Раса: {self.race}, Сила: {self.power}, Урон: {self.damage}, Уровень: {self.level},Броня: {self.armor}")
+
 
     def level_up(self,exp):
         self.exp += exp
         if self.exp == 100:
-            self.level += 100 // 20
+            self.level += 1
             self.power += 100 // 20
             self.damage += 100 // 20
             self.armor += 100 // 20
@@ -39,8 +39,9 @@ class Person:
         elif cloth == 'legendary dress':
             self.armor += 100
             self.health += 50
-        elif cloth == 'witch jacet':
+        elif cloth == 'witch jacket':
             self.mana += 100
+
         elif cloth == 'legendary plate':
             self.mana += 20
             self.armor += 500
@@ -49,21 +50,38 @@ class Person:
     def set_weapon(self,weapon):
         if weapon == 'sword':
             self.power += 1
+            self.range = 2
         elif weapon == 'axe':
             self.power += 1
             self.damage += 2
+            self.range = 2
         elif weapon == 'bow':
             self.power += 1
             self.damage += 3
-            self.mana +=  5
+            self.mana += 5
+            self.range = 100
 
 
+    def move(self):
+        self.position += 1
 
-warrior1 = Person('Tiffany',100,50,'goblin')
+    def attack(self,object):
+        if (self.position - object.position) <= self.range:
+            object.health -= self.damage
+            print("Здоровье монстра после атаки нашего героя",object.health)
+
+
+class Monster:
+
+    def __init__(self,position):
+        self.position = position
+        self.health = 20
+
+monster = Monster(2)
+
+warrior1 = Person('Tiffany','elf')
 warrior1.level_up(100)
 warrior1.wear('legendary plate')
-
-
-
-
-warrior1.description()
+warrior1.set_weapon('bow')
+warrior1.attack(monster)
+warrior1.attack(monster)
